759244a500b8f702d750483b171de820918b612d
Levente Szava <szava.levente@gmail.com>
2017-03-20 15:23:45 +0100

    Data validations and fixtures

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,63 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
+  fixtures :products
   # test "the truth" do
   #   assert true
   # end
+  test "product attributes must not be empty" do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:image_url].any?
+    assert product.errors[:price].any?
+  end
+
+  test "product price must be positive" do
+    product = Product.new(title: "Book for test",
+                          description: "Product description for test",
+                          image_url: "image.jpg")
+    product.price = -1
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"], product.errors[:price]
+
+    product.price = 0
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"], product.errors[:price]
+
+    product.price = 1
+    assert product.valid?
+  end
+
+  def new_product(image_url)
+    Product.new(title: "Book for test",
+                description: "Product description for test",
+                price:  1,
+                image_url: image_url
+                )
+  end
+
+  test "image_url" do
+    ok = %w{image.jpg image.png image.jpeg image.gif http://a.b.c/x/y/z/image.gif }
+    bad = %w{ image.doc image.gif/more image.gif.more}
+
+    ok.each do |name|
+      assert new_product(name).valid?, "#{name} shouldn't be invalid"
+    end
+
+    bad.each do |name|
+      assert new_product(name).invalid?, "#{name} shouldn't be valid"
+    end
+  end
+
+  test "product is not valid without a uique title - i18n" do
+    product = Product.new(title:  products(:product1).title,
+                          description: "xxxx",
+                          price: 1,
+                          image_url: "prod.jpg")
+    assert product.invalid?
+    assert_equal [I18n.translate('errors.messages.taken')], product.errors[:title]
+  end
+
 end
