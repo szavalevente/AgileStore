96ec3dee6f902289dea6cf9e2fd243a7081112c2
Levente Szava <szava.levente@gmail.com>
2017-03-22 10:32:26 +0100

    Modified Change to Up and Down

--- db/migrate/20170321172045_create_carts.rb
+++ db/migrate/20170321172045_create_carts.rb
@@ -1,8 +1,12 @@
 class CreateCarts < ActiveRecord::Migration[5.0]
-  def change
+  def up
     create_table :carts do |t|
 
       t.timestamps
     end
+  end
+
+  def down
+    drop_table :carts
   end
 end
--- db/migrate/20170321174230_create_line_items.rb
+++ db/migrate/20170321174230_create_line_items.rb
@@ -1,10 +1,16 @@
 class CreateLineItems < ActiveRecord::Migration[5.0]
-  def change
+  def up
     create_table :line_items do |t|
       t.references :product, foreign_key: true
       t.belongs_to :cart, foreign_key: true
 
       t.timestamps
     end
+  end
+
+  def down
+    remove_foreign_key "line_items", "carts"
+    remove_foreign_key "line_items", "products"
+    drop_table :line_items
   end
 end
