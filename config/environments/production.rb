111562d3f883c3850feb7d3330ba2dfe5f868dec
Levente Szava <szava.levente@gmail.com>
2017-03-23 14:40:42 +0100

    Condig for redis

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
+  config.web_socket_server_url = "wss://redistogo-fluffy-53772.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://redistogo-fluffy-53772.herokuapp.com', ''http://redistogo-fluffy-53772.herokuapp.com'']
+
 end
