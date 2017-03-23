42e4a2160055e83e0f0dedc9373d3c583d9d4c1b
Levente Szava <szava.levente@gmail.com>
2017-03-23 13:37:40 +0100

    Set Ruby version 2.4.0 in Production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,6 +83,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  config.action_cable.disable_request_forgery_protection = true
 end
