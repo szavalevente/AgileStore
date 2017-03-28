c88981c07eac3880e2121778dfbe58f919b54736
Levente Szava <szava.levente@gmail.com>
2017-03-28 14:58:36 +0200

    Login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,18 @@
 Rails.application.routes.draw do
+  get 'admin' => 'admin#index'
+
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+
+  resources :users
   resources :orders
   resources :line_items
   resources :carts
   root 'store#index', as: 'store_index'
+  root 'admin#index', as: 'admin_index'
 
   resources :products do
     get :who_bought, on: :member
